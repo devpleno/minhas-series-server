1ae69776ead2da788d83af9f8332e2a6d059cdf4
Tulio Faria <tuliofaria@gmail.com>
2019-07-30 20:26:21 -0300

    Fix api.

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,14 @@ router.get('/', (req, res) => res.send({
   datetime: new Date()
 }))
 
-router.use('/series', series)
-router.use('/genres', genres)
+const api = require('express').Router()
+api.get('/', (req, res) => res.send({
+  info: 'Minhas Séries Server',
+  datetime: new Date()
+}))
+api.use('/series', series)
+api.use('/genres', genres)
+
+router.use('/api', api)
 
 module.exports = router
