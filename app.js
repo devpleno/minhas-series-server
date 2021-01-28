e8a358ca6acb502d4245122ef5fdbf6b169866af
Luccas Pio <64063580+DevPio@users.noreply.github.com>
2021-01-28 09:26:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cors = require('cors')
 
 const bodyParser = require('body-parser')
+app.use(cors())
 app.use(bodyParser.json({ extended: true }))
 
 const routes = require('./routes')
